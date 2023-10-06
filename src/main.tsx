8431f5ba654b30bb2f3cd819ffaaa932f7e4e59e
Gustavo Marchesim <105670464+gustavomarchesim@users.noreply.github.com>
2023-10-06 17:54:14 -0300

    config de sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import { Login } from './view/Login.tsx';
+
+import './assets/styles/globals.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

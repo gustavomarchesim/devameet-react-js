24e2ae4c38771bd5fe8a0b168cdb8c659e7ce9b8
Gustavo Marchesim <105670464+gustavomarchesim@users.noreply.github.com>
2023-10-09 15:19:12 -0300

    Visual Login, Desk e Mobile finalizado

--- src/view/Login.tsx
+++ src/view/Login.tsx
@@ -1,3 +1,42 @@
+import logo from '../assets/images/logo.svg';
+import emailIcon from '../assets/images/mail.svg';
+import senhaIcon from '../assets/images/key.svg';
+
 export const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className='container-public'>
+      <img
+        src={logo}
+        alt='Logo Devameet'
+        className='logo'
+      />
+      <form>
+        <div className='input'>
+          <img
+            src={emailIcon}
+            alt='Logo email'
+          />
+          <input
+            type='text'
+            placeholder='E-mail'
+          />
+        </div>
+        <div className='input'>
+          <img
+            src={senhaIcon}
+            alt='Logo senha'
+          />
+          <input
+            type='password'
+            placeholder='Senha'
+          />
+        </div>
+        <button type='button'>Login</button>
+        <div className='link'>
+          <p>Não possui uma conta?</p>
+          <a href=''>Faça seu cadastro agora</a>
+        </div>
+      </form>
+    </div>
+  );
 };
